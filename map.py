b260d6f9cef1223c7a5df08bc020e209a0940077
Yolwoocle <54135715+Yolwoocle@users.noreply.github.com>
2023-01-21 17:11:44 +0100

    Map

--- map.py
+++ map.py
@@ -1,44 +1,72 @@
+import pygame
 from engine.slimyengine import *
 
+class Tile:
+    def __init__(self, name:str) -> None:
+        """Tile represents a tile in the map.
+
+        Args:
+            name (str): Name of the Tile.
+        """
+        
+        self.name = name
+
+
+
 class Map(Drawable):
-    def __init__(self, w:int, h:int, tile_size:int) -> None:
+    def __init__(self, size:vec3, tile_size:int) -> None:
+        """Map that stores data about the world.
+
+        Args:
+            size (vec3): Size of the map area.
+            tile_size (int): Size of a tile of the map.
+        """
         Drawable.__init__(self)
         
-        self.width = w
-        self.height = h
+        self.size = size
         self.tile_size = tile_size
         
-        self.grid = [[None for i in range(h)] for j in range(w)]
+        self.grid = [[[None for i in range(size.z)] for j in range(size.y)] for j in range(size.x)]
         self.dynamic_tiles = []
         
-        self.texture = ...
+        self.map_surface = pygame.Surface((size.x * tile_size, size.y * tile_size))
         
-    def in_bounds(self, x:int, y:int):
+        
+    def in_bounds(self, x:int, y:int) -> bool:
         return (0 <= x < self.width and 0 <= y < self.height)
+        
         
     def get(self, x:int, y:int):
         if not self.in_bounds(x, y):
             return None
         return self.grid[x][y]
 
-    def set(self, x:int, y:int, tile):
+
+    def set(self, x:int, y:int, tile:Tile):
         if not self.in_bounds(x, y):
             return False
         
         old_obj = self.grid[x][y]
         
         self.grid[x][y] = tile
-        if type(tile) != type:
+        if tile.is_dynamic:
             self.dynamic_tiles.append(tile)
         
-        if type(old_obj) != type:
+        if old_obj.is_dynamic:
             old_obj._delete_me = True 
             
         return old_obj
     
 
-    def tick(self):
-        ...
+    def tick(self, dt):
+        for tile in self.dynamic_tiles: 
+            tile.tick()
         
-        # TODO: go through all dynamic tiles & update them, delete if needed
+    def draw(self, screen):
+        for z in range(self.size.z):
+            for y in range(self.size.y):
+                for x in range(self.size.x):
+                    self.draw_tile(screen, self.grid[x][y][z], x, y, z)
 
+    def draw_tile(self, screen, tile, x, y, z):
+        tile.texture.blit(screen, self.tile_size * vec2(x, y))
